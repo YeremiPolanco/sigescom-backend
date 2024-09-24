88e5f27a403b77f865926b7143fc0e03bf4eb934
YeremiPolanco <yeremi.polanco.julca@gmail.com>
2024-09-24 00:59:47 -0500

    implement data to authResponse

--- src/main/java/ms/jiren/sigescom/securiry/service/AuthService.java
+++ src/main/java/ms/jiren/sigescom/securiry/service/AuthService.java
@@ -24,10 +24,13 @@ public class AuthService {
 
     public AuthResponse login(LoginRequest request) {
         authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getUsername(), request.getPassword()));
-        UserDetails user = userRepository.findByUsername(request.getUsername()).orElseThrow();
-        String token = jwtService.getToken(user);
+        UserDetails userDetail = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        User user = userRepository.findByUsername(request.getUsername()).orElseThrow();
+        String token = jwtService.getToken(userDetail);
         return AuthResponse.builder()
                 .token(token)
+                .fullName(user.getFirstName() + " " + user.getLastName())
+                .role(user.getRoles().name())
                 .build();
     }
 
